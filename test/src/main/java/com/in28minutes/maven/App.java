d632d56571351ccf8ad169b99caf3e69f54f4bbe
EC2 Default User <ec2-user@ip-172-31-32-221.ec2.internal>
2018-09-04 11:26:08 +0000

    modified test and app.java

--- test/src/main/java/com/in28minutes/maven/App.java
+++ test/src/main/java/com/in28minutes/maven/App.java
@@ -4,10 +4,8 @@ package com.in28minutes.maven;
  * Hello World test!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World  Test Test Test" );
     }
 
